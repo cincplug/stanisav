d08ff5091dd1860ed3134382eaaa88d287bc74e6
Luka Stanisavljevic <luka@cincplug.com>
2025-12-24 15:13:03 +0100

    Improve loop icon

--- src/components/menu/MenuIcons.jsx
+++ src/components/menu/MenuIcons.jsx
@@ -1,81 +1,86 @@
 const iconColor = "#fff";
+const iconSize = 24;
+const viewBox = `0 0 ${iconSize} ${iconSize}`;
+const strokeWidth = 1.5;
 
 export const BurgerIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <path
       d="M3 12H21M3 6H21M3 18H21"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
     />
   </svg>
 );
 
 export const CloseIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <path
       d="M18 6L6 18M6 6L18 18"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
     />
   </svg>
 );
 
 export const PlayIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <polygon points="8,5 20,12 8,19" fill={iconColor} />
   </svg>
 );
 
 export const PauseIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <rect x="6" y="5" width="4" height="14" fill={iconColor} />
     <rect x="14" y="5" width="4" height="14" fill={iconColor} />
   </svg>
 );
 
 export const BeginIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <polygon points="7,12 17,5 17,19" fill={iconColor} />
-    <rect x="3" y="5" width="3" height="14" fill={iconColor} />
+    <rect x="3" y="5" width={strokeWidth} height="14" fill={iconColor} />
   </svg>
 );
 
 export const PrevIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <polygon points="18,5 10,12 18,19" fill={iconColor} />
     <polygon points="14,5 6,12 14,19" fill={iconColor} />
   </svg>
 );
 
 export const NextIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     <polygon points="6,5 14,12 6,19" fill={iconColor} />
     <polygon points="10,5 18,12 10,19" fill={iconColor} />
   </svg>
 );
 
 export const LoopIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width={iconSize} height={iconSize} viewBox={viewBox} fill="none">
     {/* Top semicircle arrow (clockwise) */}
     <path
-      d="M19 9C19 6.5 17 4 13 4C9 4 7 6 7 8"
+      d="M18 9C18 6.5 16 4 12 4C8 4 6 6 6 8"
       stroke={iconColor}
-      strokeWidth="2"
+      strokeWidth={strokeWidth}
       fill="none"
       strokeLinecap="round"
     />
-    <polygon points="19,9 17,6 21,7" fill={iconColor} />
+    {/* Equilateral triangle pointing down-left at end of top arc */}
+    <polygon points="9,8 3,8 6,11.5" fill={iconColor} />
 
     {/* Bottom semicircle arrow (clockwise) */}
     <path
-      d="M5 15C5 17.5 7 20 11 20C15 20 17 18 17 16"
+      d="M6 15C6 17.5 8 20 12 20C16 20 18 18 18 16"
       stroke={iconColor}
-      strokeWidth="2"
+      strokeWidth={strokeWidth}
       fill="none"
       strokeLinecap="round"
     />
-    <polygon points="5,15 7,18 3,17" fill={iconColor} />
+    {/* Equilateral triangle pointing up-right at end of bottom arc */}
+    <polygon points="15,16 21,16 18,12.5" fill={iconColor} />
   </svg>
 );
