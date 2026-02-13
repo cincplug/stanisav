9cfc9211a406d127456aae7de346551e2d889fc8
Luka Stanisavljevic <luka@cincplug.com>
2026-02-13 14:51:58 +0100

    Clean up

--- src/hooks/useAudioAnimation.js
+++ src/hooks/useAudioAnimation.js
@@ -1,16 +1,9 @@
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getAudioAnalysisService } from "../services/audioService.js";
 import { defaultAudioData } from "../config/meshaDefaultAudioData.js";
 
 export function useAudioAnimation() {
   const [audioData, setAudioData] = useState(defaultAudioData);
-
-  const rotationStateRef = useRef({
-    currentYRotation: 0,
-    targetYRotation: 0,
-    audioStartTime: 0,
-    wasAudioActive: false,
-  });
 
   useEffect(() => {
     const audioService = getAudioAnalysisService();
@@ -23,5 +16,5 @@ export function useAudioAnimation() {
     };
   }, []);
 
-  return { audioData, rotationStateRef };
+  return { audioData };
 }
