5334b4dda2484bfddef3ed3c1a06067e384deb57
Luka <luka@cincplug.com>
2026-05-28 13:36:23 +0200

    Fix regression

--- src/components/r3f/LabelsClusterTitle.jsx
+++ src/components/r3f/LabelsClusterTitle.jsx
@@ -17,7 +17,7 @@ const LabelsClusterTitle = ({ languagePositions, title }) => {
     if (titleRef.current && topCenter) {
       titleRef.current.position.set(
         topCenter.x,
-        topCenter.y + config.titleOffset,
+        topCenter.y + config.segmentation.titleOffset,
         topCenter.z,
       );
     }
