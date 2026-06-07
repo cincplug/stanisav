fa330b9f5c30e49394eb86c2897d26bc195bc436
Luka <luka@cincplug.com>
2026-06-07 12:51:26 +0200

    Improve ear surface balance

--- src/components/r3f/MeshaEars.jsx
+++ src/components/r3f/MeshaEars.jsx
@@ -66,7 +66,7 @@ const MeshaEars = ({
       <mesh
         ref={leftMeshRef}
         position={[-x, y, z]}
-        scale={[-earWidth, earHeight / (segments * 2), earDepth]}
+        scale={[-earWidth, earHeight / (segments * 2), earDepth / segments]}
         onClick={onClick}
         linguisticProperty="morphology"
         {...bind()}
@@ -76,7 +76,7 @@ const MeshaEars = ({
       <mesh
         ref={rightMeshRef}
         position={[x, y, z]}
-        scale={[earWidth, earHeight / (segments * 2), earDepth]}
+        scale={[earWidth, earHeight / (segments * 2), earDepth / segments]}
         onClick={onClick}
         linguisticProperty="morphology"
         {...bind()}
