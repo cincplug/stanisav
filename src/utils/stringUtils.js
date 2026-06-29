399119132e8d4dd28a376105c87fb1cdb3371fa7
Luka <luka@cincplug.com>
2026-06-29 14:47:29 +0200

    Fix formatting util

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -2,6 +2,8 @@ export const formatCamelCase = (str) =>
   str
     .replace(/([A-Z])/g, " $1")
     .replace(/\./g, " ")
-    .toLowerCase()
-    .replace(/^./, (c) => c.toUpperCase())
-    .trim();
+    .trim()
+    .replace(
+      /^(.)(.*)$/,
+      (_, first, rest) => first.toUpperCase() + rest.toLowerCase(),
+    );
