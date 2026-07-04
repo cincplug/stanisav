108a30011317bf52819b9bd8ff62c7fcc49a4457
Luka <luka@cincplug.com>
2026-07-04 11:55:19 +0200

    Spread teeth according to number

--- src/components/r3f/MeshaTeeth.jsx
+++ src/components/r3f/MeshaTeeth.jsx
@@ -15,38 +15,46 @@ import { shiftHue } from "../../utils/colorUtils";
 extend({ RoundedBoxGeometry });
 
 // Isolated tooth so each instance can call useShaderMaterial with its own color
-const MeshaTooth = forwardRef(
-  ({ tooth, centerIndex, color, isSelected, onClick }, ref) => {
-    const { config } = useConfigContext();
-    const { toothWidth, toothLength, toothThickness, toothRoundness } = config;
-    const toothMaterial = useShaderMaterial(color);
-    const highlightMaterial = useHighlightMaterial(0, 2);
+const MeshaTooth = forwardRef(({ tooth, color, isSelected, onClick }, ref) => {
+  const { config } = useConfigContext();
+  const {
+    toothWidth,
+    toothLengthCentral,
+    toothThicknessCentral,
+    toothLengthCorner,
+    toothThicknessCorner,
+  } = config;
+  const { indexFromCenter, halfCount } = tooth;
 
-    return (
-      <group
-        position={[tooth.x, tooth.y, tooth.z - 1]}
-        rotation={[tooth.rotationAngle, 0, 0]}
-      >
-        <mesh ref={ref} linguisticProperty="phonemeCount" onClick={onClick}>
-          <roundedBoxGeometry
-            args={[
-              toothWidth,
-              toothLength / (centerIndex + 1),
-              toothThickness * centerIndex,
-              6,
-              toothRoundness,
-            ]}
-          />
-          {isSelected ? (
-            <shaderMaterial args={[highlightMaterial]} />
-          ) : (
-            <shaderMaterial args={[toothMaterial]} />
-          )}
-        </mesh>
-      </group>
-    );
-  },
-);
+  const toothMaterial = useShaderMaterial(color);
+  const highlightMaterial = useHighlightMaterial(0, 2);
+
+  const ratio = halfCount > 0 ? indexFromCenter / halfCount : 0;
+  const length =
+    toothLengthCentral + (toothLengthCorner - toothLengthCentral) * ratio;
+  const thickness =
+    toothThicknessCentral +
+    (toothThicknessCorner - toothThicknessCentral) * ratio;
+  const roundness = thickness / 2;
+
+  return (
+    <group
+      position={[tooth.x, tooth.y, tooth.z - 1]}
+      rotation={[tooth.rotationAngle, 0, 0]}
+    >
+      <mesh ref={ref} linguisticProperty="phonemeCount" onClick={onClick}>
+        <roundedBoxGeometry
+          args={[toothWidth, length, thickness, 6, roundness]}
+        />
+        {isSelected ? (
+          <shaderMaterial args={[highlightMaterial]} />
+        ) : (
+          <shaderMaterial args={[toothMaterial]} />
+        )}
+      </mesh>
+    </group>
+  );
+});
 
 const MeshaTeeth = ({
   toothCount,
@@ -57,8 +65,15 @@ const MeshaTeeth = ({
   const teethRefs = useRef([]);
   const { audioData } = useAudioData();
   const { config } = useConfigContext();
-  const { toothColor, toothSize, toothColorStep, toothY, toothZ, toothSpin } =
-    config;
+  const {
+    toothSpacing,
+    toothColor,
+    toothSize,
+    toothColorStep,
+    toothY,
+    toothZ,
+    toothSpin,
+  } = config;
   const centerIndex = (toothCount - 1) / 2;
 
   const bind = useMeshaDrag(dragBindings.teeth, "phonemeCount");
@@ -67,27 +82,24 @@ const MeshaTeeth = ({
     if (toothCount === 0) return [];
     const arc = Math.PI;
     const startAngle = Math.PI / 2 - arc / 2;
+    const halfCount = (toothCount - 1) / 2;
+    const radius = toothCount * toothSpacing;
 
     return Array.from({ length: toothCount }, (_, toothIndex) => {
-      const angle = startAngle + (toothIndex / toothCount) * arc;
-      const positionInCluster = toothIndex % consonantClusterSize;
-      const clusterCenter = (consonantClusterSize - 1) / 2;
-      const y =
-        consonantClusterSize > 1
-          ? Math.abs(positionInCluster - clusterCenter) /
-            Math.floor(consonantClusterSize / 2)
-          : 0;
+      const angle = startAngle + (toothIndex / (toothCount - 1)) * arc;
+      const indexFromCenter = Math.abs(toothIndex - halfCount);
 
       return {
-        x: Math.cos(angle),
-        y,
-        z: Math.sin(angle) * consonantClusterSize,
-        positionInCluster,
+        x: Math.cos(angle) * radius,
+        y: 0,
+        z: Math.sin(angle) * radius,
+        indexFromCenter,
+        halfCount,
         rotationAngle: 0,
         key: `tooth-${toothIndex}`,
       };
     });
-  }, [toothCount, consonantClusterSize]);
+  }, [toothCount, toothSpacing]);
 
   useThrottledFrame(() => {
     if (teethRefs.current) {
@@ -109,17 +121,20 @@ const MeshaTeeth = ({
 
   return (
     <group position={[0, toothY, toothZ]} scale={toothSize} {...bind()}>
-      {teeth.map((tooth, i) => (
-        <MeshaTooth
-          key={tooth.key}
-          ref={(el) => (teethRefs.current[i] = el)}
-          tooth={tooth}
-          centerIndex={centerIndex}
-          color={shiftHue(toothColor, toothColorStep * i)}
-          isSelected={isSelected}
-          onClick={onClick}
-        />
-      ))}
+      {teeth.map((tooth, i) => {
+        return (
+          <MeshaTooth
+            key={tooth.key}
+            ref={(el) => (teethRefs.current[i] = el)}
+            tooth={tooth}
+            index={i}
+            centerIndex={centerIndex}
+            color={shiftHue(toothColor, toothColorStep * i)}
+            isSelected={isSelected}
+            onClick={onClick}
+          />
+        );
+      })}
     </group>
   );
 };
