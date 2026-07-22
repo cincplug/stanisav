84ecf37d9e64f59e3d47c85868ea6409364bdd15
Luka <luka@cincplug.com>
2026-07-22 08:43:01 +0200

    Fix the article path

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { I18nProvider } from "./contexts/I18nContext";
 import { LanguageColorsProvider } from "./contexts/LanguageColorsContext.jsx";
 import { LanguageSelectionProvider } from "./contexts/LanguageSelectionContext";
 import { PlaylistProvider } from "./contexts/PlaylistContext";
-import Article from "./pages/Article/Article.jsx";
+import Article from "./pages/article/Article.jsx";
 import SourceVideoGallery from "./pages/source-video-gallery/SourceVideoGallery.jsx";
 
 import LocaleLayout from "./components/routing/LocaleLayout.jsx";
